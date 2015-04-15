a2b39cd6bfc41c8b5e419b78e6612875461848da
Nate M <Mr.awsmness@gmail.com>
2015-04-15 10:53:39 -0700

    milestone 7

---
+++ Includes/connect.php
@@ -0,0 +1,16 @@
+<?php
+//localhost is what it connects to, root and root are the username and password, tasks is the table
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+
+//if mysqli doesn't work then it will die and give an error message
+if($mysqli->connect_error) {
+	die('connection error (' . $mysqli->connect_errno .  ')'
+		. $mysqli->connect_error);
+}
+else{
+	echo "Connection made";
+}
+		
+$mysqli->close();	
+
+?>
